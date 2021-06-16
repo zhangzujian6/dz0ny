e0409a038978c185bafa2ff675c8815c0ea3ed78
zhangzujian6 <fumeixianz128@163.com>
2021-06-16 20:54:52 +0800

    3

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         <li>3</li>
         <li>4</li>
       </ul>
-      <div class="box">按钮</div>
+      <div class="box">搜索框</div>
       <div class="login-btn">
         <div class="btn">登录</div>
         |
@@ -32,31 +32,129 @@
             <li>音乐</li>
             <li>音乐</li>
           </ul>
-          <div class="more">更多<img src="../public/right.png" alt=""></div>
+          <div class="more">更多<img src="../public/right.png" alt="" /></div>
         </div>
         <div class="line"></div>
         <div class="books">
-          <div class="book"><img src="../public/book.jpg" alt="" />
-          <p>神话故事：蜗牛故事有声版</p></div>
-          <div class="book"><img src="../public/book.jpg" alt="" />
-          <p>神话故事：蜗牛故事有声版</p></div>
-          <div class="book"><img src="../public/book.jpg" alt="" />
-          <p>神话故事：蜗牛故事有声版</p></div>
-          <div class="book"><img src="../public/book.jpg" alt="" />
-          <p>神话故事：蜗牛故事有声版</p></div>
-          <div class="book"><img src="../public/book.jpg" alt="" />
-          <p>神话故事：蜗牛故事有声版</p></div>
-          <div class="book"><img src="../public/book.jpg" alt="" />
-          <p>神话故事：蜗牛故事有声版</p></div>
-          <div class="book"><img src="../public/book.jpg" alt="" />
-          <p>神话故事：蜗牛故事有声版</p></div>
-          <div class="book"><img src="../public/book.jpg" alt="" />
-          <p>神话故事：蜗牛故事有声版</p></div>
+          <div class="book">
+            <img src="../public/book.jpg" alt="" />
+            <p>神话故事：蜗牛故事有声版</p>
+          </div>
+          <div class="book">
+            <img src="../public/book.jpg" alt="" />
+            <p>神话故事：蜗牛故事有声版</p>
+          </div>
+          <div class="book">
+            <img src="../public/book.jpg" alt="" />
+            <p>神话故事：蜗牛故事有声版</p>
+          </div>
+          <div class="book">
+            <img src="../public/book.jpg" alt="" />
+            <p>神话故事：蜗牛故事有声版</p>
+          </div>
+          <div class="book">
+            <img src="../public/book.jpg" alt="" />
+            <p>神话故事：蜗牛故事有声版</p>
+          </div>
+          <div class="book">
+            <img src="../public/book.jpg" alt="" />
+            <p>神话故事：蜗牛故事有声版</p>
+          </div>
+          <div class="book">
+            <img src="../public/book.jpg" alt="" />
+            <p>神话故事：蜗牛故事有声版</p>
+          </div>
+          <div class="book">
+            <img src="../public/book.jpg" alt="" />
+            <p>神话故事：蜗牛故事有声版</p>
+          </div>
         </div>
-    <div style="height:20px;"></div>
+        <div style="height: 20px"></div>
+      </div>
+      <div class="slider">
+        <div class="slider-title">热门作者</div>
+        <div class="more2">更多</div>
+        <img class="more-icon" src="../public/right.png" alt="" />
+        <div class="sliders">
+          <div class="slider-item">
+            <div class="number">
+              <p>01</p>
+              <div class="number-line"></div>
+            </div>
+            <div class="portrait">
+              <img src="../public/photo2.png" alt="" />
+            </div>
+            <div class="info">
+              <div class="name">
+                <p>有声的子衿</p><img class="vip" src="../public/VIP.png" alt="" />
+              </div>
+              <div class="brief">人肉故事机。来呀，等你关注</div>
+            </div>
+          </div>
+<div class="slider-item">
+            <div class="number">
+              <p>01</p>
+              <div class="number-line"></div>
+            </div>
+            <div class="portrait">
+              <img src="../public/photo2.png" alt="" />
+            </div>
+            <div class="info">
+              <div class="name">
+                <p>有声的子衿</p><img class="vip" src="../public/VIP.png" alt="" />
+              </div>
+              <div class="brief">人肉故事机。来呀，等你关注</div>
+            </div>
+          </div>
+          <div class="slider-item">
+            <div class="number">
+              <p>01</p>
+              <div class="number-line"></div>
+            </div>
+            <div class="portrait">
+              <img src="../public/photo2.png" alt="" />
+            </div>
+            <div class="info">
+              <div class="name">
+                <p>有声的子衿</p><img class="vip" src="../public/VIP.png" alt="" />
+              </div>
+              <div class="brief">人肉故事机。来呀，等你关注</div>
+            </div>
+          </div>
+          <div class="slider-item">
+            <div class="number">
+              <p>01</p>
+              <div class="number-line"></div>
+            </div>
+            <div class="portrait">
+              <img src="../public/photo2.png" alt="" />
+            </div>
+            <div class="info">
+              <div class="name">
+                <p>有声的子衿</p><img class="vip" src="../public/VIP.png" alt="" />
+              </div>
+              <div class="brief">人肉故事机。来呀，等你关注</div>
+            </div>
+          </div>
+          <div class="slider-item">
+            <div class="number">
+              <p>01</p>
+              <div class="number-line"></div>
+            </div>
+            <div class="portrait">
+              <img src="../public/photo2.png" alt="" />
+            </div>
+            <div class="info">
+              <div class="name">
+                <p>有声的子衿</p><img class="vip" src="../public/VIP.png" alt="" />
+              </div>
+              <div class="brief">人肉故事机。来呀，等你关注</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div style="height:50px;"></div>
+    <div style="height: 50px"></div>
   </div>
 </template>
 
@@ -122,13 +220,13 @@ select {
   cursor: pointer;
 }
 .box {
-  width: 100px;
-  height: 50px;
+  width: 300px;
+  height: 28px;
   background-color: #fff;
   border-radius: 12px;
   margin-left: 28px;
   text-align: center;
-  line-height: 50px;
+  line-height: 28px;
 }
 .login-btn {
   width: 120px;
@@ -156,6 +254,7 @@ select {
 .main {
   width: 1180px;
   margin: 0 auto;
+  display: flex;
 }
 .main .title1 {
   font-size: 22px;
@@ -223,5 +322,75 @@ select {
 .books .book P {
   font-size: 14px;
   margin: 8px 6px;
+}
+.slider {
+  position: relative;
+  width: 380px;
+  border: 1px solid #ccc;
+  padding: 0 20px;
+}
+.slider-item {
+  position: relative;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+}
+.slider-item:first-child {
+border-top: 2px solid #ccc;
+}
+.slider-title {
+  font-size: 22px;
+  font-weight: 500;
+  margin-top: 12px;
+  margin-bottom: 15px;
+}
+.more2 {
+  position: absolute;
+  right: 40px;
+  top: 16px;
+}
+.more-icon {
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  width: 14px;
+  height: 14px;
+}
+.slider-item .number {
+  font-size: 28px;
+}
+.slider-item .number-line {
+  width: 32px;
+  height: 2px;
+  background-color: #ccc;
+  margin-top: 8px;
+}
+.slider-item .portrait {
+  margin-left: 20px;
+}
+.slider-item .portrait img {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+}
+.slider-item .info {
+  margin-left: 20px;
+}
+.slider-item .info .name {
+  display: flex;
+  font-size: 16px;
+  font-weight: 600;
+  align-items: center;
+  margin-top: -10px;
+}
+.slider-item .info .brief {
+  font-size: 12px;
+  color: rgb(138, 137, 137);
+}
+.slider-item .vip {
+  width: 40px;
+  height: 38px;
 }
 </style>
