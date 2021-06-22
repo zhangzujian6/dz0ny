d35facbc072c5bcbed10c391c729111d249a7b4d
zhangzujian6 <fumeixianz128@163.com>
2021-06-23 06:02:22 +0800

    5

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,10 +1,63 @@
 <template>
   <div class="player-container">
-    <div>
-      <button @click="toPlay">点击播放</button>
+    <div class="main">
+      <!-- <button @click="toPlay">点击播放</button>
       <button @click="toPause">点击暂停</button>
       <button @click="change">切换</button>
-      <button @click="play">切换</button>
+      <button @click="play">切换</button> -->
+      <div class="CD">
+        <div class="CD-bg" :class="type === 2 ? 'circle' : ''">
+          <img src="../../public/book.jpg" alt="" />
+        </div>
+        <div class="CD-info">
+          <ul>
+            <li style="font-size: 40px; list-style-type: none; margin-left: 0">
+              蜗牛故事
+            </li>
+            <li>作者：有声的子衿</li>
+            <li>合集：神话故事</li>
+            <li>发布时间：2022-03-19</li>
+            <li>评分：9.5</li>
+          </ul>
+        </div>
+      </div>
+      <div class="controls">
+        <img src="../../public/last.png" alt="" />
+        <img
+          @click="toPlay"
+          v-if="type === 1"
+          src="../../public/play2.png"
+          alt=""
+        />
+        <img
+          @click="toPause"
+          v-if="type === 2"
+          src="../../public/pause2.png"
+          alt=""
+        />
+        <img src="../../public/next.png" alt="" />
+      </div>
+      <div class="comments">
+        <div class="comments-title">
+          <p>发表我的评论</p>
+        </div>
+        <div class="comments-content">
+          <textarea
+            name=""
+            id=""
+            cols="80"
+            rows="3"
+            style="outline: none;font-size:22px"
+          ></textarea>
+          <button>发表</button>
+        </div>
+        <div style="height:14px"></div>
+        <hr>
+        <div class="comments-title">
+          <p>全部评论(1w+)</p>
+        </div>
+        <div></div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,30 +66,118 @@
 export default {
   data() {
     return {
+      type: 1,
       user: "mybbidh",
     };
   },
   methods: {
     toPlay() {
       this.$parent.$refs.MusicPlay.play();
+      this.type = 2;
     },
     toPause() {
       this.$parent.$refs.MusicPlay.pause();
+      this.type = 1;
     },
     change() {
       console.log("logout child");
       this.$emit("logoutUser", this.user);
     },
-    play(){
-
-    }
+    play() {},
   },
 };
 </script>
 
 <style scoped>
-.player {
-  width: 600px;
-  height: 60px;
+.main {
+  width: 1180px;
+  height: 600px;
+  margin: 0 auto;
+  border: 1px solid #ccc;
+}
+.CD {
+  position: relative;
+  width: 100%;
+  height: 500px;
+  padding-top: 50px;
+}
+.CD-bg {
+  width: 400px;
+  height: 400px;
+  margin-top: 50px;
+  background-color: #303132;
+  border-radius: 50%;
+  border: 20px solid #d3d3d3;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.circle {
+  animation: circle 12s linear infinite;
+}
+.CD-bg img {
+  width: 260px;
+  height: 260px;
+  border-radius: 50%;
+}
+@keyframes circle {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  25% {
+    -webkit-transform: rotate(90deg);
+  }
+  50% {
+    -webkit-transform: rotate(180deg);
+  }
+  75% {
+    -webkit-transform: rotate(270deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+.controls {
+  width: 400px;
+  height: 100px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.controls img {
+  cursor: pointer;
+}
+.CD-info {
+  position: absolute;
+  top: 100px;
+  right: 50px;
+}
+.CD-info ul li {
+  margin-left: 16px;
+  margin-top: 8px;
+  color: #535455;
+}
+.comments {
+  border: 1px solid #ccc;
+  padding: 20px 100px;
+}
+.comments-title p {
+  font-size: 18px;
+  font-weight: 600;
+}
+.comments-content {
+  display: flex;
+  margin-top: 20px;
+}
+.comments-content button {
+  width: 60px;
+  height: 40px;
+  color: white;
+  background-color: rgb(97, 94, 94);
+  border: none;
+  margin-top: 40px;
+  margin-left: 20px;
 }
 </style>> 
